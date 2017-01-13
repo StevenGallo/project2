fed53ef38f7115d803a06127b6881dcb0f6814ab
Steven Gallo <steven.gallo@outook.com>
2017-01-13 14:16:17 -0500

    added and imported components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import MovieSearch from './components/MovieSearch';
+import MovieList from './components/MovieList';
+import Header from './components/Header';
+import Party from './components/Party';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
       </div>
     );
   }
